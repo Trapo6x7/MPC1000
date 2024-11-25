227fbd5ce47ffef90ed439707877294770be912a
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-25 23:09:04 +0100

    OBJECT?

--- script.js
+++ script.js
@@ -21,17 +21,18 @@ function handleKeyUp(event) {
   audio.currentTime = 0;
   audio.play();
   
-  let playedTime = Date.now(event);
+  /* let playedTime = Date.now(event);
   console.log(playedTime);
   let objTimePlayed = {
     calulateValueNotes: function () {
+      currentNote.key = event.key;
+      currentNote.time = playedTime
       let currentYear = new Date().getUTCFullYear();
       let notes = currentYear - this.playedTime;
       console.log(notes);
     },
   };
   let currentNote = Object.create(objTimePlayed);
-  currentNote.key = event.key;
-  currentNote.time = playedTime
-  console.log(objTimePlayed)
+
+  console.log(objTimePlayed) */
 }
