b0d653bfbb25b3a7caec0e129f6d54bef8472f67
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-25 23:06:33 +0100

    ADD:OBJECT?

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 document.addEventListener("keydown", handleKeyDown);
 document.addEventListener("keyup", handleKeyUp);
-const start = Date.now()
-console.log(start);
 
 // date.now chaque notes pour savoir le ryhtme
 // mettre ces nombres dans un tableau
 // lire le tableau
 
 function handleKeyDown(event) {
-    const keyCode = event.keyCode;
-    const key = document.querySelector(`.key[data-key = '${keyCode}']`);
-    if (!key) return;
-    key.classList.add("playing");
-    console.log("hello")
+  const keyCode = event.keyCode;
+  const key = document.querySelector(`.key[data-key = '${keyCode}']`);
+  if (!key) return;
+  key.classList.add("playing");
 }
 
 function handleKeyUp(event) {
-    const keyCode = event.keyCode;
-    const audio = document.querySelector(`audio[data-key = '${keyCode}']`);
-    const key = document.querySelector(`.key[data-key = '${keyCode}']`);
-    if (!key) return;
-    key.classList.remove("playing");
-     audio.currentTime = 0;
-    audio.play();
-} 
-
-
+  const keyCode = event.keyCode;
+  const audio = document.querySelector(`audio[data-key = '${keyCode}']`);
+  const key = document.querySelector(`.key[data-key = '${keyCode}']`);
+  if (!key) return;
+  key.classList.remove("playing");
+  audio.currentTime = 0;
+  audio.play();
+  
+  let playedTime = Date.now(event);
+  console.log(playedTime);
+  let objTimePlayed = {
+    calulateValueNotes: function () {
+      let currentYear = new Date().getUTCFullYear();
+      let notes = currentYear - this.playedTime;
+      console.log(notes);
+    },
+  };
+  let currentNote = Object.create(objTimePlayed);
+  currentNote.key = event.key;
+  currentNote.time = playedTime
+  console.log(objTimePlayed)
+}
