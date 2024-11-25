17ef7fd9d2819896d664640b03cb7de958d15167
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-25 22:12:14 +0100

    ADD: CURRENTTIME

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 document.addEventListener("keydown", handleKeyDown);
 document.addEventListener("keyup", handleKeyUp);
+const start = Date.now()
+console.log(start);
+
+// date.now chaque notes pour savoir le ryhtme
+// mettre ces nombres dans un tableau
+// lire le tableau
 
 function handleKeyDown(event) {
     const keyCode = event.keyCode;
     const key = document.querySelector(`.key[data-key = '${keyCode}']`);
     if (!key) return;
     key.classList.add("playing");
+    console.log("hello")
 }
 
 function handleKeyUp(event) {
@@ -14,5 +21,8 @@ function handleKeyUp(event) {
     const key = document.querySelector(`.key[data-key = '${keyCode}']`);
     if (!key) return;
     key.classList.remove("playing");
+     audio.currentTime = 0;
     audio.play();
-}
+} 
+
+
