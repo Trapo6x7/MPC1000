f2a325ea049fea092329eb27a0a2503f55e06228
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-26 09:05:56 +0100

    test

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ document.addEventListener("keydown", handleKeyDown);
 document.addEventListener("keyup", handleKeyUp);
 
 function handleKeyDown(event) {
-    const keyCode = event.keyCode;
-    const key = document.querySelector(`.key[data-key = '${keyCode}']`);
-    if (!key) return;
-    key.classList.add("playing");
+  const keyCode = event.keyCode;
+  const key = document.querySelector(`.key[data-key = '${keyCode}']`);
+
+  if (key.dataset.clicked === "true") return;
+  key.dataset.clicked = "true";
+  if (!key) return;
+  key.classList.add("playing");
+  const audio = document.querySelector(`audio[data-key = '${keyCode}']`);
+  audio.play();
 }
 
 function handleKeyUp(event) {
-    const keyCode = event.keyCode;
-    const audio = document.querySelector(`audio[data-key = '${keyCode}']`);
-    const key = document.querySelector(`.key[data-key = '${keyCode}']`);
-    if (!key) return;
-    key.classList.remove("playing");
-    audio.play();
+  const keyCode = event.keyCode;
+  const key = document.querySelector(`.key[data-key = '${keyCode}']`);
+  if (!key) return;
+  key.dataset.clicked = "false";
+  key.classList.remove("playing");
 }
