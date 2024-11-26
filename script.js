e1ea50e6227925a6d3b14688a780792d456e1924
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-26 09:06:42 +0100

    yo'

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 document.addEventListener("keydown", handleKeyDown);
 document.addEventListener("keyup", handleKeyUp);
 
+// date.now chaque notes pour savoir le ryhtme
+// mettre ces nombres dans un tableau
+// lire le tableau
+
 function handleKeyDown(event) {
   const keyCode = event.keyCode;
   const key = document.querySelector(`.key[data-key = '${keyCode}']`);
-
-  if (key.dataset.clicked === "true") return;
-  key.dataset.clicked = "true";
   if (!key) return;
   key.classList.add("playing");
-  const audio = document.querySelector(`audio[data-key = '${keyCode}']`);
-  audio.play();
 }
 
 function handleKeyUp(event) {
   const keyCode = event.keyCode;
+  const audio = document.querySelector(`audio[data-key = '${keyCode}']`);
   const key = document.querySelector(`.key[data-key = '${keyCode}']`);
   if (!key) return;
-  key.dataset.clicked = "false";
   key.classList.remove("playing");
+  audio.currentTime = 0;
+  audio.play();
+  
+  /* let playedTime = Date.now(event);
+  console.log(playedTime);
+  let objTimePlayed = {
+    calulateValueNotes: function () {
+      currentNote.key = event.key;
+      currentNote.time = playedTime
+      let currentYear = new Date().getUTCFullYear();
+      let notes = currentYear - this.playedTime;
+      console.log(notes);
+    },
+  };
+  let currentNote = Object.create(objTimePlayed);
+
+  console.log(objTimePlayed) */
 }
