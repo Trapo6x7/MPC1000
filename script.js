d0fdd6b2b6ec98bfa94f287c2fd0461f58a69d57
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-26 09:08:41 +0100

    object

--- script.js
+++ script.js
@@ -13,10 +13,26 @@ function handleKeyDown(event) {
 }
 
 function handleKeyUp(event) {
-    const keyCode = event.keyCode;
-    const audio = document.querySelector(`audio[data-key = '${keyCode}']`);
-    const key = document.querySelector(`.key[data-key = '${keyCode}']`);
-    if (!key) return;
-    key.classList.remove("playing");
-    audio.play();
+  const keyCode = event.keyCode;
+  const audio = document.querySelector(`audio[data-key = '${keyCode}']`);
+  const key = document.querySelector(`.key[data-key = '${keyCode}']`);
+  if (!key) return;
+  key.classList.remove("playing");
+  audio.currentTime = 0;
+  audio.play();
+  
+  /* let playedTime = Date.now(event);
+  console.log(playedTime);
+  let objTimePlayed = {
+    calulateValueNotes: function () {
+      currentNote.key = event.key;
+      currentNote.time = playedTime
+      let currentYear = new Date().getUTCFullYear();
+      let notes = currentYear - this.playedTime;
+      console.log(notes);
+    },
+  };
+  let currentNote = Object.create(objTimePlayed);
+
+  console.log(objTimePlayed) */
 }
